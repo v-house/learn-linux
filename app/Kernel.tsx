43087970020f13117c02ef3e196ce0471b13f74f
Vivek <365anu65@gmail.com>
2023-11-06 01:21:28 +0530

    Design - 1...

---
+++ app/Kernel.tsx
@@ -0,0 +1,9 @@
+export default function Kernel() {
+  return (
+    <>
+      <main className="max-w-full h-full flex relative overflow-y-hidden">
+        <div className="bg-slate-500 w-full h-full"></div>
+      </main>
+    </>
+  );
+}
---
+++ app/Navbar.tsx
@@ -0,0 +1,16 @@
+import { AiFillGithub, AiOutlineInstagram } from "react-icons/ai";
+
+export default function Navv() {
+  return (
+    <>
+      <aside className="h-full w-16 flex flex-col space-y-10 items-center justify-center relative bg-gray-800 text-white">
+        <div className="h-10 w-10 flex items-center justify-center rounded-lg text-2xl cursor-pointer hover:text-gray-800 hover:bg-white hover:duration-300 hover:ease-linear focus:bg-white">
+          <AiOutlineInstagram />
+        </div>
+        <div className="h-10 w-10 text-2xl flex items-center justify-center rounded-lg cursor-pointer hover:text-gray-800 hover:bg-white hover:duration-300 hover:ease-linear focus:bg-white">
+          <AiFillGithub />
+        </div>
+      </aside>
+    </>
+  );
+}
