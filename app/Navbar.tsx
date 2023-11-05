eef1fc01ae417e1a87f781218bc86a00bac56def
Vivek <365anu65@gmail.com>
2023-11-06 01:30:04 +0530

    Improving top navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -3,7 +3,7 @@ import { AiFillGithub, AiOutlineInstagram } from "react-icons/ai";
 export default function Navv() {
   return (
     <>
-      <aside className="h-full w-16 flex flex-col space-y-10 items-center justify-center relative bg-gray-800 text-white">
+      <aside className="h-full w-16 flex flex-col space-y-10 items-center justify-center relative bg-slate-900 text-white">
         <div className="h-10 w-10 flex items-center justify-center rounded-lg text-2xl cursor-pointer hover:text-gray-800 hover:bg-white hover:duration-300 hover:ease-linear focus:bg-white">
           <AiOutlineInstagram />
         </div>
--- app/TopNav.tsx
+++ app/TopNav.tsx
@@ -1,26 +1,23 @@
 export default function Topnav() {
   return (
     <>
-      <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+      <nav className="flex items-center justify-between bg-teal-500 p-6">
         <div className="flex items-center flex-shrink-0 text-white mr-6">
-          <span className="text-4xl mr-4">!{">"}</span>
+          <span className="text-4xl mr-4">{"!>"}</span>
           <span className="font-semibold text-xl tracking-tight">
-            Learn Linux
+            Try Linux
           </span>
         </div>
         <div className="">
           <div className="">
-            <a
-              href="#responsive-header"
-              className="text-teal-200 hover:text-white mr-4 flex"
-            >
+            <a href="#responsive-header" className="text-white flex">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
                 strokeWidth={1.5}
                 stroke="currentColor"
-                className="w-6 h-6 mr-2"
+                className="w-8 h-8 mr-2"
               >
                 <path
                   strokeLinecap="round"
@@ -28,7 +25,6 @@ export default function Topnav() {
                   d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75"
                 />
               </svg>
-              Try these!!
             </a>
           </div>
         </div>
